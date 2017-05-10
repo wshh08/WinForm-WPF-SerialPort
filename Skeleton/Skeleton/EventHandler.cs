ef1aa742cec0adebc163baa99cdbb873f7b93c5f
SLKyrim <623962644@qq.com>
2017-05-10 18:07:27 +0800

    Skeleton上位机
    开始做串口选择模块功能

---
+++ Skeleton/Skeleton/EventHandler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO.Ports;
+
+namespace Skeleton
+{
+
+    public partial class MainWindow : Window
+    {
+        //事件类
+        private SerialPort motor_SerialPort;
+        private SerialPort press_SerialPort;
+        private SerialPort angle_SerialPort;
+
+        private void Motor_comboBox_DropDownClosed(object sender, EventArgs e)
+        {
+            //电机及控制串口下拉窗口关闭后执行
+            ComboBoxItem item = Motor_comboBox.SelectedItem as ComboBoxItem; //下拉窗口当前选中的项赋给item
+            string tempstr = item.Content.ToString();                        //将选中的项目转为字串存储在tempstr中
+
+            for (int i = 0; i < SPCount.Length; i++)
+            {
+                if(tempstr == "串口" + SPCount[i])
+                {
+                    //当选中串口为串口SPCount[i]时
+                    if(press_com == SPCount[i] || angle_com == SPCount[i])
+                    {
+                        //压力与倾角传感器或倾角传感器已占用串口SPCount[i]时
+                        MessageBox.Show("串口" + SPCount[i] + "已被占用!");
+                    }
+                    else
+                    {
+                        motor_com = SPCount[i];
+
+                        if (motor_SerialPort.IsOpen)   //如果电机正在使用串口，则关闭串口以备初始化
+                            motor_SerialPort.Close();
+
+                        motor_SerialPort = new SerialPort();
+                        motor_SerialPort.PortName = SPCount[i];
+                        motor_SerialPort.BaudRate = 115200;
+                        motor_SerialPort.Parity = Parity.None;
+                        motor_SerialPort.StopBits = StopBits.One;
+                        motor_SerialPort.Open();
+                        motor_SerialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(controlPort_DataReceived);
+                    }
+                }
+            }
+        }
+
+        private void controlPort_DataReceived(object sender, SerialDataReceivedEventArgs e)//压力倾角串口接收代码
+        {
+            try
+            {
+                int bufferlen = motor_SerialPort.BytesToRead;//先记录下来，避免某种原因，人为的原因，操作几次之间时间长，缓存不一致
+                if (bufferlen >= 27)
+                {
+                    byte[] bytes = new byte[bufferlen];//声明一个临时数组存储当前来的串口数据
+                    motor_SerialPort.Read(bytes, 0, bufferlen);//读取串口内部缓冲区数据到buf数组
+                    motor_SerialPort.DiscardInBuffer();//清空串口内部缓存
+                    //处理和存储数据
+                    Int16 endFlag = BitConverter.ToInt16(bytes, 25);
+                    if (endFlag == 2573)
+                    {
+                        if (bytes[0] == 0x23)
+                            for (int f = 0; f < 4; f++)
+                            {
+                                enable[f] = bytes[f * 6 + 1];
+                                direction[f] = bytes[f * 6 + 2];
+                                speed[f] = bytes[f * 6 + 3] * 256 + bytes[f * 6 + 4];
+                                if (speed[f] >= 2048) speed[f] = (speed[f] - 2048) / 4096 * 5180;
+                                else speed[f] = (2048 - speed[f]) / 4096 * -5180;
+                                current[f] = bytes[f * 6 + 5] * 256 + bytes[f * 6 + 6];
+                                if (current[f] >= 2048) current[f] = (current[f] - 2048) / 4096 * 30;
+                                else current[f] = (2048 - current[f]) / 4096 * -30;
+                            }
+                    }
+                }
+            }
+            catch
+            {
+
+
+            }
+        }
+    }
+}
---
+++ Skeleton/Skeleton/Method.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO.Ports;
+
+namespace Skeleton
+{
+    //方法类
+    public partial class MainWindow : Window
+    {
+        public string[] SPCount = null;           //用来存储计算机串口名称数组
+        public int comcount = 0;                  //用来存储计算机可用串口数目，初始化为0
+        public string motor_com = null;           //存储电机所用串口
+        public string press_com = null;           //存储压力及倾角传感器所用串口
+        public string angle_com = null;           //存储角度传感器所用串口
+        public bool flag = false;                 
+
+        public void ScanPorts()
+        {
+            //扫描可用串口
+            SPCount = SerialPort.GetPortNames();      //获得计算机可用串口名称数组
+            ComboBoxItem tempComboBoxItem = new ComboBoxItem();   
+            if(comcount != SPCount.Length)            //SPCount.length其实就是可用串口的个数
+            {
+                //当可用串口计数器与实际可用串口个数不相符时
+                //初始化三个下拉窗口并将flag初始化为false
+
+                Motor_comboBox.Items.Clear();
+                Press_comboBox.Items.Clear();
+                Angle_comboBox.Items.Clear();
+
+                tempComboBoxItem = new ComboBoxItem();
+                tempComboBoxItem.Content = "请选择串口";
+                Motor_comboBox.Items.Add(tempComboBoxItem);
+                Motor_comboBox.SelectedIndex = 0;
+
+                tempComboBoxItem = new ComboBoxItem();
+                tempComboBoxItem.Content = "请选择串口";
+                Press_comboBox.Items.Add(tempComboBoxItem);
+                Press_comboBox.SelectedIndex = 0;
+
+                tempComboBoxItem = new ComboBoxItem();
+                tempComboBoxItem.Content = "请选择串口";
+                Angle_comboBox.Items.Add(tempComboBoxItem);
+                Angle_comboBox.SelectedIndex = 0;
+
+                motor_com = null;
+                press_com = null;
+                angle_com = null;
+
+                flag = false;
+
+                if(comcount != 0)
+                {
+                    //在操作过程中增加或减少串口时发生
+                    MessageBox.Show("串口数目已改变，请重新选择串口");
+                }
+
+                comcount = SPCount.Length;     //将可用串口计数器与现在可用串口个数匹配
+            }
+
+            if(!flag)
+            {
+                if(SPCount.Length > 0)
+                {
+                    //有可用串口时执行
+                    comcount = SPCount.Length;
+
+                    Out_textBox.Text = "检测到" + SPCount.Length + "个串口!";
+
+                    for (int i = 0; i < SPCount.Length; i++)
+                    {
+                        //分别将可用串口添加到三个下拉窗口中
+                        string tempstr = "串口" + SPCount[i];
+
+                        tempComboBoxItem = new ComboBoxItem();
+                        tempComboBoxItem.Content = tempstr;
+                        Motor_comboBox.Items.Add(tempComboBoxItem);
+
+                        tempComboBoxItem = new ComboBoxItem();
+                        tempComboBoxItem.Content = tempstr;
+                        Press_comboBox.Items.Add(tempComboBoxItem);
+
+                        tempComboBoxItem = new ComboBoxItem();
+                        tempComboBoxItem.Content = tempstr;
+                        Angle_comboBox.Items.Add(tempComboBoxItem);
+                    }
+
+                    flag = true;
+
+                }
+                else
+                {
+                    comcount = 0;
+                    Out_textBox.Text = "未检测到串口!";
+                }
+            }
+        }
+
+    }
+}
